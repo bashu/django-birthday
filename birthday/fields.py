b55e072f37a357fb47bd45c2b8fdd0db947be93e
Basil Shubin <basil.shubin@gmail.com>
2017-01-18 10:46:29 +0700

    Update fields.py

--- birthday/fields.py
+++ birthday/fields.py
@@ -13,12 +13,17 @@ def pre_save_listener(instance, **kwargs):
     setattr(instance, field.doy_name, doy)
 
 class BirthdayField(DateField):        
+
     def contribute_to_class(self, cls, name):
         super(BirthdayField, self).contribute_to_class(cls, name)
+
         if hasattr(cls._meta, 'birthday_field'):
             raise FieldError("django-birthday does not support multiple "
                              "BirthdayFields on a single model.")
         cls._meta.birthday_field = self
+
         self.doy_name = '%s_dayofyear_internal' % name
-        internal_field.contribute_to_class(cls, self.doy_name)
+        if not hasattr(cls, self.doy_name):
+            internal_field.contribute_to_class(cls, self.doy_name)
+
         pre_save.connect(pre_save_listener, sender=cls)
