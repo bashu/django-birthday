8a3ee13b1f864bad4288e2ddfa24ab7dfd9399ec
Basil Shubin <basil.shubin@gmail.com>
2017-01-20 16:08:27 +0700

    code clean-up (once again)…

--- birthday/fields.py
+++ birthday/fields.py
@@ -4,15 +4,14 @@ from django.core.exceptions import FieldError
 from django.db.models.fields import DateField, PositiveSmallIntegerField
 from django.db.models.signals import pre_save
 
-internal_field = PositiveSmallIntegerField(editable=False, default=None, null=True)
-
 
 def pre_save_listener(instance, **kwargs):
-    field = instance._meta.birthday_field
-    birthday = getattr(instance, field.name)
+    field_obj = instance._meta.birthday_field
+
+    birthday = getattr(instance, field_obj.name)
     if not birthday:
         return
-    setattr(instance, field.doy_name, birthday.timetuple().tm_yday)
+    setattr(instance, field_obj.doy_name, birthday.timetuple().tm_yday)
 
 
 class BirthdayField(DateField):        
@@ -21,12 +20,12 @@ class BirthdayField(DateField):
         super(BirthdayField, self).contribute_to_class(cls, name)
 
         if hasattr(cls._meta, 'birthday_field'):
-            raise FieldError("django-birthday does not support multiple BirthdayFields on a single model")
-
+            raise FieldError(
+                "django-birthday does not support multiple BirthdayFields on a single model")
         cls._meta.birthday_field = self
 
         self.doy_name = '%s_dayofyear_internal' % name
         if not hasattr(cls, self.doy_name):
-            internal_field.contribute_to_class(cls, self.doy_name)
+            PositiveSmallIntegerField(editable=False, default=None, null=True).contribute_to_class(cls, self.doy_name)
 
         pre_save.connect(pre_save_listener, sender=cls)
