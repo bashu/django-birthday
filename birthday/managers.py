7a2e741d77090e762032426f1e0db0a44c53996a
Basil Shubin <basil.shubin@gmail.com>
2017-01-20 15:52:29 +0700

    code clean-up…

--- birthday/managers.py
+++ birthday/managers.py
@@ -1,9 +1,14 @@
-from birthday.fields import BirthdayField
+# -*- coding: utf-8 -*-
+
 from datetime import date
-from django.db.models.manager import Manager
+
+from django.db import models
 from django.db.models.query_utils import Q
 
+from .fields import BirthdayField
+
 CASE = "CASE WHEN %(bdoy)s<%(cdoy)s THEN %(bdoy)s+365 ELSE %(bdoy)s END"
+
 
 def _order(manager, reverse=False, case=False):
     cdoy = date.today().timetuple().tm_yday
@@ -19,7 +24,9 @@ def _order(manager, reverse=False, case=False):
         return qs.order_by('-%s' % order_field)
     return qs.order_by('%s' % order_field)
 
-class BirthdayManager(Manager):    
+
+class BirthdayManager(models.Manager):    
+
     @property
     def _birthday_doy_field(self):
         return self.model._meta.birthday_field.doy_name
@@ -29,27 +36,27 @@ class BirthdayManager(Manager):
             day = date.today()
         return day.timetuple().tm_yday
             
-    def get_upcoming_birthdays(self, days=30, after=None, include_day=True,
-            order=True, reverse=False):
+    def get_upcoming_birthdays(self, days=30, after=None, include_day=True, order=True, reverse=False):
         today = self._doy(after)
         limit = today + days
         q = Q(**{'%s__gt%s' % (self._birthday_doy_field, 'e' if include_day else ''): today})
         q &= Q(**{'%s__lt' % self._birthday_doy_field: limit})
+
         if limit > 365:
             limit = limit - 365
             today = 1
             q2 = Q(**{'%s__gte' % self._birthday_doy_field: today})
             q2 &= Q(**{'%s__lt' % self._birthday_doy_field: limit})
             q = q | q2
+
         if order:
             qs = _order(self, reverse, True)
             return qs.filter(q)
-        else:
-            return self.filter(q)
+
+        return self.filter(q)
     
     def get_birthdays(self, day=None):
-        today = self._doy(day)
-        return self.filter(**{self._birthday_doy_field: today})
+        return self.filter(**{self._birthday_doy_field: self._doy(day)})
     
     def order_by_birthday(self, reverse=False):
         return _order(self, reverse)
