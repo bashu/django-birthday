2a5c198ab17a43da6d7a72f4cb7a70b4531b88a3
Basil Shubin <144783+bashu@users.noreply.github.com>
2026-08-17 16:15:51 +0600

    Simplify test settings

--- birthday/tests/settings.py
+++ birthday/tests/settings.py
@@ -1,33 +1,11 @@
-SECRET_KEY = "YOUR_SECRET_KEY"  # noqa: S105
+SECRET_KEY = "DUMMY_SECRET_KEY"  # noqa: S105
 
 # Application definition
 
 PROJECT_APPS = ["birthday.tests", "birthday"]
 
 INSTALLED_APPS = [
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
     *PROJECT_APPS,
-]
-
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.contrib.auth.context_processors.auth",
-                "django.template.context_processors.debug",
-                "django.template.context_processors.i18n",
-                "django.template.context_processors.media",
-                "django.template.context_processors.request",
-                "django.template.context_processors.static",
-                "django.template.context_processors.tz",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
 ]
 
 # Database
